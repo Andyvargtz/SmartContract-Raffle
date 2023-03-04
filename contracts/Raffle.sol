285f70c20c60d9a5019bd41329178c3e3ba6363c
andrea <andyvargtz@gmail.com>
2023-03-04 17:15:34 -0600

    first commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,13 @@
+//Raffle Contracts
+
+//Enter the lottery (Pay)
+//Pick a random winner (Verifiable random) -> Chainlink Oracle
+//Winner be selected every x minutes (Automate) -> Chainlink Keepers
+
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract Raffle {
+    //function enterRaffle(){}
+    //function pickRandomWinner(){}
+}
